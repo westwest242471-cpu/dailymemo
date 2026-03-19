858f527bbb9c4910abdde20d88b7ec91ac5036e0
westwest242471-cpu <westwest242471@gmail.com>
2026-03-19 14:38:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-memo-v1.2';
+const CACHE_NAME = 'daily-memo-v1.3';
 const urlsToCache = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
