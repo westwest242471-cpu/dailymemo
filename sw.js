ee38ac3cb83dabaa04a910526bd819eee157d8e7
westwest242471-cpu <westwest242471@gmail.com>
2026-03-19 14:48:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
-const CACHE_NAME = 'daily-memo-v1.3';
-const urlsToCache = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'daily-memo-v9'; // 更新時はここをv10, v11と変える
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// インストール時にファイルを保存
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
+// 古いキャッシュを掃除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 【重要】ネットワーク優先、ダメならキャッシュ
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
